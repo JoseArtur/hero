37b5dee94214717006ac74b5d60d53aedeae8e18
José Artur <65734671+JoseArtur@users.noreply.github.com>
2021-11-02 15:40:55 +0000

    Created a new class called Game

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,2 +1,20 @@
+import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.screen.Screen;
+import com.googlecode.lanterna.screen.TerminalScreen;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.Terminal;
+
+import java.io.IOException;
+
 public class Game {
-}
+    public Game(){
+        try {
+            Terminal terminal = new DefaultTerminalFactory().createTerminal();
+            Screen screen = new TerminalScreen(terminal);
+            screen.setCursorPosition(null); // we don't need a cursor
+            screen.startScreen(); // screens must be started
+            screen.doResizeIfNecessary(); // resize screen if necessary
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+}}
